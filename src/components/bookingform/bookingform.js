2b398a8e3cf21ef20e67f49b9141484bbbd41692
Gustavo Jordan <gjordanrad@gmail.com>
2025-10-12 15:01:07 -0500

    added form state and validation

--- src/components/bookingform/bookingform.js
+++ src/components/bookingform/bookingform.js
@@ -1,36 +1,88 @@
-import "./bookingform.css"; 
+import "./bookingform.css";
 import { useState } from "react";
-
-const [resDate, setResDate] = useState[""];
-const [resTime, setResTime] = useState[""];
-const [gest, setGuest] = useState[0];
-const [occasion, setOccasion] = useState[""];
+import { validateEmail } from "../../utils/utils";
 
 export const Bookingform = () => {
+    const [resDate, setResDate] = useState("");
+    const [resTime, setResTime] = useState("");
+    const [guest, setGuest] = useState(1);
+    const [occasion, setOccasion] =useState("");
+    const availableTimes = ["15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+    const timeOptions = availableTimes.map(time => {
+        return <option>{time}</option>
+    });
+
+    const getIsFormValid = () => {
+        return validateEmail("gus@gmail.com");
+    };
+
+    const clearForm = () => {
+        setResDate("");
+        setResTime("");
+        setGuest(1);
+        setOccasion("");
+    };
+
+    const handleSumit = (e) => {
+        e.preventDefault();
+        alert("Thanks for your reservation!"); 
+        clearForm();
+    }
+
     return (
-    <div className="formcontain">
-        <h2>Please fill the information for your reservation:</h2>
-        <form className="bookingform">
-            <label htmlForfor="res-date">Choose date</label>
-            <input type="date" id="res-date"/>
-            <label htmlFor="res-time">Choose time</label>
-            <select id="res-time ">
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests"/>
-            <label htmlFor="occasion">Occasion</label>
-            <select id="occasion">
-                <option>Birthday</option>
-                <option>Anniversary</option>
-            </select>
-            <input type="submit" value="Make Your reservation" />
+      <div className="formcontain">
+        <h2>Request your reservation, Please fill the next form:</h2>
+        <form className="bookingform" onSubmit={handleSumit}>
+          <label htmlFor="res-date">Choose date</label>
+          <input
+            type="date"
+            id="res-date"
+            value={resDate}
+            onChange={(e) => {
+              setResDate(e.target.value);
+            }}
+          />
+          <label htmlFor="res-time">Choose time</label>
+          <select
+            id="res-time"
+            value={resTime}
+            onChange={(e) => {
+              setResTime(e.target.value);
+            }}
+          >
+            {timeOptions}
+          </select>
+          <label htmlFor="guests">Number of guests</label>
+          <input
+            type="number"
+            placeholder="1"
+            min="1"
+            max="10"
+            id="guests"
+            value={guest}
+            onChange={(e) => {
+              setGuest(e.target.value);
+            }}
+          />
+          <label htmlFor="occasion">Occasion</label>
+          <select
+            id="occasion"
+            value={occasion}
+            onChange={(e) => {
+              setOccasion(e.target.value);
+            }}
+          >
+            <option>Birthday</option>
+            <option>Anniversary</option>
+          </select>
+          <button
+            type="submit"
+            value="Make Your reservation"
+            disabled={!getIsFormValid()}
+          >
+            Make Your reservation
+          </button>
         </form>
-    </div>
+      </div>
     );
 };
